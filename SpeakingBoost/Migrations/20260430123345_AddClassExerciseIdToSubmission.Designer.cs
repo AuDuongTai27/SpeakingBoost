a9db06dbbe432e5a2ac1bcc09b58dc2987bae846
TaiAu <auduongtai27@gmail.com>
2026-04-30 19:36:32 +0700

    Update migration

--- SpeakingBoost/Migrations/20260430123345_AddClassExerciseIdToSubmission.Designer.cs
+++ SpeakingBoost/Migrations/20260430123345_AddClassExerciseIdToSubmission.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SpeakingBoost.Models.EF;
 
@@ -11,9 +12,11 @@ using SpeakingBoost.Models.EF;
 namespace SpeakingBoost.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260430123345_AddClassExerciseIdToSubmission")]
+    partial class AddClassExerciseIdToSubmission
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
