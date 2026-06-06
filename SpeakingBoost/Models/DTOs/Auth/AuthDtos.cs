160a68d7faaf7470f750757f9408c87664c0dced
Duong Tan Phat <phat.duongtan.cit22@eiu.edu.vn>
2026-06-06 18:52:41 +0700

    feat: add new collumn CreatedAt to User entity. Migrate to database. Modifiy DTO and service. Make the joined date display.

--- SpeakingBoost/Models/DTOs/Auth/AuthDtos.cs
+++ SpeakingBoost/Models/DTOs/Auth/AuthDtos.cs
@@ -64,7 +64,6 @@ namespace SpeakingBoost.Models.DTOs.Auth
         public string FullName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
-
-
+        public DateTime CreatedAt { get; set; }
     }
 }
